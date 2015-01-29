38aeb201d07b7902489650c1405d3e588c6bcc5c
Virus17 <virus17@users.noreply.github.com>
2015-01-29 12:22:05 +0530

    Added comments in Strategy Pattern

--- src/com/virus17/designpatthern/strategy/ClientApp.java
+++ src/com/virus17/designpatthern/strategy/ClientApp.java
@@ -13,7 +13,16 @@ import java.util.List;
  *  In future, if we want to add HeapSort, none of the existing classes needs to be 
  *  modified(close for modification), we can create a new class which implement SortingStrategy
  *  and we are done adding a new Algorithm (Open for Extension)
- *  s 
+ *  
+ *  JDK has couple of examples of this pattern, first is Collection.sort(List, Comparator) 
+ *  method, where Comparator is Strategy and Collections.sort() is Sorter(in our example). 
+ *  Because of this pattern your sort method can sort any object, the object which doesn't exists 
+ *  when this method was written. As long as, Object will implement Comparator interface 
+ *  (Strategy interface), Collections.sort() method will sort it. 
+ *  
+ *  Another example is  java.util.Arrays#sort(T[], Comparator < ? super T > c) method 
+ *  which similar to Collections.sort() method, except need array in place of List.
+ *  
  * @author virus17
  */
 public class ClientApp {
